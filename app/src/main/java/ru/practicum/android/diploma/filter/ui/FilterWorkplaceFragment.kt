bbfcb2c13f8113c1c17c972341d4433575f1f1c6
Саид <HUEY.news@yandex.ru>
2024-06-07 22:52:08 +0300

    GH#21: Создал фрагменты экранов и макеты с заглушками для них.

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/FilterWorkplaceFragment.kt
@@ -0,0 +1,18 @@
+package ru.practicum.android.diploma.filter.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentFilterWorkplaceBinding
+
+class FilterWorkplaceFragment : Fragment() {
+    private var _binding: FragmentFilterWorkplaceBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentFilterWorkplaceBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
