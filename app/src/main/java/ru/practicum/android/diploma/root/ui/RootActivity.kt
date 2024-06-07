48d335ae5755c74279b3058b0aeb866619fcf703
Саид <HUEY.news@yandex.ru>
2024-06-08 00:16:41 +0300

    GH#20: Реализовал bottomNavigation в соответствии с макетом.

--- app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -1,8 +1,15 @@
 package ru.practicum.android.diploma.root.ui
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
+import java.util.Locale
 
 class RootActivity : AppCompatActivity() {
     private var _binding: ActivityRootBinding? = null
@@ -12,5 +19,42 @@ class RootActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigation = binding.bottomNavigation
+
+        bottomNavigation.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.filterCountryFragment,
+                R.id.filterIndustryFragment,
+                R.id.filterSettingsFragment,
+                R.id.filterWorkplaceFragment,
+                R.id.filterRegionFragment,
+                R.id.vacancyDetailsFragment ->
+                    bottomNavigation.isVisible = false
+                else ->
+                    bottomNavigation.isVisible = true
+
+            }
+        }
+
+        bottomNavigation.setupWithNavController(navController)
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(updateLocale(base))
+        applyOverrideConfiguration(base.resources.configuration)
+    }
+
+    private fun updateLocale(context: Context): Context? {
+        val ruLocale = Locale("ru")
+        Locale.setDefault(ruLocale)
+        val configuration: Configuration = context.resources.configuration
+        configuration.setLocale(ruLocale)
+        configuration.setLayoutDirection(ruLocale)
+        return context.createConfigurationContext(configuration)
     }
 }
