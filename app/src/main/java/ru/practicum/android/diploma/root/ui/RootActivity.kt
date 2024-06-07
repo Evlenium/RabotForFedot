ff38f1015dc139e9ef7ba4385e6b08c6f336dc9f
Саид <HUEY.news@yandex.ru>
2024-06-07 23:39:02 +0300

    GH#21: Скорректировал файл RootActivity.

--- app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -2,20 +2,15 @@ package ru.practicum.android.diploma.root.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.practicum.android.diploma.BuildConfig
-import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+    private var _binding: ActivityRootBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
-
-        // Пример использования access token для HeadHunter API
-        networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
+        _binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
-    private fun networkRequestExample(accessToken: String) {
-        // ...
-    }
-
 }
