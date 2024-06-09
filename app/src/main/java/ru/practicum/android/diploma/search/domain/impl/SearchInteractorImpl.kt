9d44ae2ad26174c3f7654c41cf3d313c395d7ba0
Vladislav <gudsanta66@gmail.com>
2024-06-09 17:38:37 +0300

    -GH#35: исправления для собаки

--- app/src/main/java/ru/practicum/android/diploma/search/domain/impl/SearchInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/impl/SearchInteractorImpl.kt
@@ -1,11 +1,11 @@
 package ru.practicum.android.diploma.search.domain.impl
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import ru.practicum.android.diploma.search.domain.SearchInteractor
 import ru.practicum.android.diploma.search.domain.SearchRepository
 import ru.practicum.android.diploma.search.domain.model.Vacancy
 import ru.practicum.android.diploma.search.data.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class SearchInteractorImpl(private val searchRepository: SearchRepository) : SearchInteractor {
     override suspend fun searchVacancies(
