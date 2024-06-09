6de7466182fbf252ac2189b725523e6f77c17102
Evlenium <Evlenium@mail.ru>
2024-06-09 19:25:21 +0300

    Dog Relax

--- app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchRepository.kt
@@ -5,5 +5,8 @@ import ru.practicum.android.diploma.search.domain.model.ServerVacanciesResponse
 import ru.practicum.android.diploma.util.Resource
 
 interface SearchRepository {
-    suspend fun searchVacancies(expression: String, filters: Map<String, String>): Flow<Resource<ServerVacanciesResponse>>
+    suspend fun searchVacancies(
+        expression: String,
+        filters: Map<String, String>
+    ): Flow<Resource<ServerVacanciesResponse>>
 }
