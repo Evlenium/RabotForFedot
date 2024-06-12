3af3d08f9bd10338bc82258aabd81f6a742953a0
NikishSl <134765116+NikishSl@users.noreply.github.com>
2024-06-12 18:02:22 +0700

    GH#53 Верстка фрагмента фильтрации

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/FilterSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/FilterSettingsFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilterSettingsBinding
 
 class FilterSettingsFragment : Fragment() {
@@ -13,6 +15,19 @@ class FilterSettingsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentFilterSettingsBinding.inflate(inflater, container, false)
+        setupToolbar()
         return binding.root
+    }
+
+    private fun setupToolbar() {
+        (activity as? AppCompatActivity)?.setSupportActionBar(binding.filtrationVacancyToolbar)
+        (activity as? AppCompatActivity)?.supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.icon_back)
+        }
+        binding.filtrationVacancyToolbar.setTitleTextAppearance(requireContext(), R.style.ToolbarAppStyle)
+        binding.filtrationVacancyToolbar.setNavigationOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
     }
 }
