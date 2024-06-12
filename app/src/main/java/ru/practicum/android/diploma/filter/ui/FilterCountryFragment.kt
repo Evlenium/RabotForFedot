090558f6027a34681bc96e493c5f54ddb145a131
NikishSl <134765116+NikishSl@users.noreply.github.com>
2024-06-12 18:59:18 +0700

    GH#55 Верстка фрагмента выбора страны

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/FilterCountryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/FilterCountryFragment.kt
@@ -15,4 +15,9 @@ class FilterCountryFragment : Fragment() {
         _binding = FragmentFilterCountryBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonBack.setOnClickListener { parentFragmentManager.popBackStack() }
+    }
 }
