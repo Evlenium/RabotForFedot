51205a7abb2e1958411b118c0a087d0158ff9cb3
Саид <HUEY.news@yandex.ru>
2024-06-12 12:05:15 +0300

    GH#44: Прописал Room в DI.

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,12 +1,15 @@
 package ru.practicum.android.diploma.di
 
+import androidx.room.Room
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.practicum.android.diploma.convertor.DbConverter
 import ru.practicum.android.diploma.convertor.ModelsConvertor
+import ru.practicum.android.diploma.favorite.data.db.AppDatabase
 import ru.practicum.android.diploma.search.data.network.NetworkClient
 import ru.practicum.android.diploma.search.data.network.RetrofitNetworkClient
 import ru.practicum.android.diploma.search.data.network.SearchAPI
@@ -41,6 +44,12 @@ val dataModule = module {
             )
             .build()
             .create(SearchAPI::class.java)
+    }
+
+    single {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db")
+            .fallbackToDestructiveMigration()
+            .build()
     }
 
     single<NetworkClient> { RetrofitNetworkClient(service = get(), resourceProvider = get()) }
