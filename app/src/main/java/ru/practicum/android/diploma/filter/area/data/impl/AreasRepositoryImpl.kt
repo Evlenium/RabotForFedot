722f8266e56e1b90183605537b1f9cba20e88cbe
Vladislav <gudsanta66@gmail.com>
2024-06-17 22:21:18 +0300

    -GH#73: Data и domain cлой фильтра «Место работы» 2 часть

--- app/src/main/java/ru/practicum/android/diploma/filter/area/data/impl/AreasRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/area/data/impl/AreasRepositoryImpl.kt
@@ -12,6 +12,7 @@ import ru.practicum.android.diploma.util.Resource
 
 class AreasRepositoryImpl(private val networkClient: NetworkClient, private val resourceProvider: ResourceProvider) :
     AreasRepository {
+
     override suspend fun getAreas(): Flow<Resource<List<ParentArea>>> = flow {
         val retrofitResponse = networkClient.doSearchAreaRequest()
         val searchAreaResponseList = networkClient.doSearchAreaRequest().body()
@@ -30,6 +31,55 @@ class AreasRepositoryImpl(private val networkClient: NetworkClient, private val 
             Constants.SERVER_ERROR -> emit(Resource.Error(resourceProvider.getErrorServer()))
             Constants.CONNECTION_ERROR -> emit(Resource.Error(resourceProvider.getErrorInternetConnection()))
             else -> emit(Resource.Error(resourceProvider.getErrorServer()))
+        }
+    }
+
+    override suspend fun getAllRegions(): Flow<Resource<List<ChildArea>>> = flow {
+        val childAreaList = mutableListOf<ChildArea>()
+        getAreas().collect { resourceListParentArea ->
+            if (resourceListParentArea.message != null) {
+                emit(Resource.Error(message = resourceListParentArea.message))
+            } else {
+                resourceListParentArea.data?.forEach { parentArea ->
+                    if (parentArea.id != "1001") {
+                        parentArea.childAreas?.let { childAreaList.addAll(it) }
+                    }
+                }
+                emit(Resource.Success(data = childAreaList))
+            }
+        }
+    }
+
+    override suspend fun getRegions(parentId: String): Flow<Resource<List<ChildArea>>> = flow {
+        getAllRegions().collect { resourceChildAreaList ->
+            if (resourceChildAreaList.message != null) {
+                emit(Resource.Error(message = resourceChildAreaList.message))
+            } else {
+                val childAreaListFilterByParent = mutableListOf<ChildArea>()
+                resourceChildAreaList.data?.forEach { childArea ->
+                    if (childArea.parentId.equals(parentId)) {
+                        childAreaListFilterByParent.add(childArea)
+                    }
+                }
+                // не уверен что здесь необходим asReversed
+                emit(Resource.Success(data = childAreaListFilterByParent.asReversed()))
+            }
+        }
+    }
+
+    override suspend fun getOtherRegionsCountries(parentId: String): Flow<Resource<List<ChildArea>>> = flow {
+        val otherRegionsCountriesList = mutableListOf<ChildArea>()
+        getAreas().collect { resourceListParentArea ->
+            if (resourceListParentArea.message != null) {
+                emit(Resource.Error(message = resourceListParentArea.message))
+            } else {
+                resourceListParentArea.data?.forEach { parentArea ->
+                    if (parentArea.id == parentId) {
+                        parentArea.childAreas?.let { otherRegionsCountriesList.addAll(it) }
+                    }
+                }
+                emit(Resource.Success(data = otherRegionsCountriesList))
+            }
         }
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/filter/area/domain/impl/AreasInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/area/domain/impl/AreasInteractorImpl.kt
@@ -4,12 +4,40 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import ru.practicum.android.diploma.filter.area.domain.api.AreasInteractor
 import ru.practicum.android.diploma.filter.area.domain.api.AreasRepository
+import ru.practicum.android.diploma.filter.area.domain.model.ChildArea
 import ru.practicum.android.diploma.filter.area.domain.model.ParentArea
 import ru.practicum.android.diploma.util.Resource
 
 class AreasInteractorImpl(private val areasRepository: AreasRepository) : AreasInteractor {
     override suspend fun getAreas(): Flow<Pair<List<ParentArea>?, String?>> {
         return areasRepository.getAreas().map { resource ->
+            when (resource) {
+                is Resource.Success -> Pair(resource.data, null)
+                is Resource.Error -> Pair(null, resource.message)
+            }
+        }
+    }
+
+    override suspend fun getAllRegions(): Flow<Pair<List<ChildArea>?, String?>> {
+        return areasRepository.getAllRegions().map { resource ->
+            when (resource) {
+                is Resource.Success -> Pair(resource.data, null)
+                is Resource.Error -> Pair(null, resource.message)
+            }
+        }
+    }
+
+    override suspend fun getRegions(parentId: String): Flow<Pair<List<ChildArea>?, String?>> {
+        return areasRepository.getRegions(parentId).map { resource ->
+            when (resource) {
+                is Resource.Success -> Pair(resource.data, null)
+                is Resource.Error -> Pair(null, resource.message)
+            }
+        }
+    }
+
+    override suspend fun getOtherRegionsCountries(parentId: String): Flow<Pair<List<ChildArea>?, String?>> {
+        return areasRepository.getOtherRegionsCountries(parentId).map { resource ->
             when (resource) {
                 is Resource.Success -> Pair(resource.data, null)
                 is Resource.Error -> Pair(null, resource.message)
