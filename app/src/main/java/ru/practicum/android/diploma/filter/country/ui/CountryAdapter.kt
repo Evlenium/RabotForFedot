d6ce948d4060ea5ce39dfca6b6bf2fe438fc36c0
Саид <HUEY.news@yandex.ru>
2024-07-02 13:21:50 +0300

    GH#133: Убрал diffUtil из регион адаптера, каунтри и отрасли. Убрал старый Никитин дубль кликлисенера во вьюхолдере и адаптере индустрий.

--- app/src/main/java/ru/practicum/android/diploma/filter/country/ui/CountryAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/country/ui/CountryAdapter.kt
@@ -1,26 +1,26 @@
 package ru.practicum.android.diploma.filter.country.ui
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import ru.practicum.android.diploma.databinding.ItemCountryBinding
 import ru.practicum.android.diploma.search.domain.model.Country
 
 class CountryAdapter(
-    private val itemClickListener: ItemClickListener,
-) : ListAdapter<Country, RecyclerView.ViewHolder>(CountryDiffCallBack()) {
+    private val itemClickListener: ItemClickListener
+) : RecyclerView.Adapter<CountryViewHolder>() {
 
     private val areas: MutableList<Country> = mutableListOf()
 
+    @SuppressLint("NotifyDataSetChanged")
     fun setItems(items: List<Country>) {
         areas.clear()
         areas.addAll(items.toMutableList())
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
         val layoutInspector = LayoutInflater.from(parent.context)
         return CountryViewHolder(ItemCountryBinding.inflate(layoutInspector, parent, false)) { position: Int ->
             if (position != RecyclerView.NO_POSITION) {
@@ -31,25 +31,13 @@ class CountryAdapter(
         }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        areas.getOrNull(position)?.let { area ->
-            (holder as CountryViewHolder).bind(area)
-        }
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        areas.getOrNull(position)?.let { holder.bind(it) }
     }
 
     override fun getItemCount() = areas.size
 
     fun interface ItemClickListener {
         fun onItemClick(country: Country)
-    }
-
-    class CountryDiffCallBack : DiffUtil.ItemCallback<Country>() {
-        override fun areItemsTheSame(oldItem: Country, newItem: Country): Boolean {
-            return oldItem == newItem
-        }
-
-        override fun areContentsTheSame(oldItem: Country, newItem: Country): Boolean {
-            return oldItem == newItem
-        }
     }
 }
